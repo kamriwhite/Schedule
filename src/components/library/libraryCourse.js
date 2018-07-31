29d096d0f3bc4ac308db81456df42720738753ad
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 11:58:18 -0600

    finishing at home

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -46,8 +46,13 @@ class LibraryCourse extends Component {
           {Icon("fas fa-check", "library-course__icon")}
         </div>
         <div className="library-course__line"></div>
-        <Arrow callback={status => this.handleCallback(status)} id={this.props.id} className="library-course__arrow" />
-        <Action onClick={() => this.props.toggleEnrolled(this.props.id)}className="library-course__action"/>
+        <Arrow 
+          callback={status => this.handleCallback(status)} 
+          id={this.props.id} 
+          className="library-course__arrow" />
+        <Action 
+          onClick={() => this.props.toggleEnrolled(this.props.id)}
+          className="library-course__action"/>
         { this.renderDescription() }
       </div>
     );

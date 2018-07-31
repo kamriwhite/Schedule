76244661c4a7f5b1696af56aaba43f5e75b265da
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 11:41:57 -0600

    styled addRemoveAction

--- src/components/action.js
+++ src/components/action.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import Icon from './icon';
+class Action extends component {
 
-class Action extends Component {
+  handleAction = function() {
+      this.props.onClick()
+
+      // document.getElementbyID('action').classList.add('')
+  }.bind(this);
+
   render() {
-    return (
-      <button onClick={() => this.props.onClick()} className={'${this.props.className} action'}>
-        { Icon('fas fa-plus-circle') }
-        {/* fas fa-times-circle */}
-      </button>
-    )
+      return (
+          <a
+              id='action'
+              onClick={() => this.handleAction()}
+              className={`${this.props.className} action`}>
+          </a>
+      )
   }
 }
-
-export default Action;

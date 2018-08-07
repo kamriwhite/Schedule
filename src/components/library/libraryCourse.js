fc3198ec230590b326092e7b1ebbe093709603b3
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-07 10:49:32 -0600

    personalized

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,37 +1,37 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 import Icon from "../icon";
 import Arrow from "../arrow";
 import Action from "../action";
 
-import AnimateHeight from 'react-animate-height';
+import AnimateHeight from "react-animate-height";
 
 class LibraryCourse extends Component {
- constructor(props) {
-   super(props);
+  constructor(props) {
+    super(props);
 
-   this.setState = {
-     status: true,
-     height: 0
-   };
- }
+    this.state = {
+      status: true,
+      height: 0
+    };
+  }
 
- handleCallback = function(status) {
-   let height = this.state.height == 0 ? 'auto' : 0;
-   if(!status) {
-     document.getElementById('library-course').classList.add('library-course-selected');
-   } else {
-     document.getElementById(this.id).classList.remove('library-course-selected');
-   }
-   this.setState({
-     status,
-     height
-   });
- }.bind(this);
+  handleCallback = function(status) {
+    let height = this.state.height == 0 ? 'auto' : 0;
+    if (!status) {
+      document.getElementById(this.id).classList.add("library-course-selected");
+    } else {
+      document.getElementById(this.id).classList.remove("library-course-selected");
+    }
+    this.setState({ 
+      status,
+      height
+    });
+  }.bind(this);
 
-render() {
+  render() {
     this.id = `library-course-${this.props.id}`;
     return (
       <div id={this.id} className="library-course">
@@ -54,14 +54,13 @@ render() {
           height={this.state.height}
         >
           <div className="library-course__description">
-          <label>Course Description</label>
-          <p>{ this.props.description }</p>
+            <label>Course Description</label>
+            <p>{this.props.description}</p>
           </div>
         </AnimateHeight>
-
       </div>
     );
   }
 }
 
-export default connect(null, actions)(LibraryCourse); 
+export default connect(null, actions)(LibraryCourse);
